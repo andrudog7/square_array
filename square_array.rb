f88ac3861f350097e42a43d5fff18d2de5c62f8e
--add <andrewksmoker@gmail.com>
2020-10-20 16:28:10 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  new_arr = []
+  array.each do |number|
+    new_arr.push((number ** 2))
+  end 
+  return new_arr
 end
+
+# def square_array(array)
+#   array.collect {|number| number ** 2}
+# end
